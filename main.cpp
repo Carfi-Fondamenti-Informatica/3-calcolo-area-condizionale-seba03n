36a0130d66fb70f6c7de09d1597b2d037960ea33
seba03n <115528575+seba03n@users.noreply.github.com>
2022-11-19 13:58:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   
+   float a,b ;
+   int opzione;
+   
+   cin >> a >> b >> opzione;
+   swith (opzione){
+   case 0:
+      cout << (a*b)/2 << endl;
+      break;
+      
+   case 1:
+      cout << a*a << endl;
+      break;
+      
+   case 2:
+      cout << a*b << endl;
+      break;
+      
+   }
    return 0;
 }
