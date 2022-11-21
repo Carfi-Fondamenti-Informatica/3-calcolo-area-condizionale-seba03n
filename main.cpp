85db4a1b0b6569efbff5cf525ec3c3c361dacd41
seba03n <115528575+seba03n@users.noreply.github.com>
2022-11-21 08:18:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,20 +7,17 @@ int main() {
    int opzione;
    
    cin >> a >> b >> opzione;
-   swith (opzione){
+   switch (opzione){
    case 0:
       cout << ((a*b)/2);
       break;
-      
    case 1:
       cout << (a*a);
       break;
-      
    case 2:
       cout << (a*b);
       break;
-      
-    default :
+    default:
       cout << "opzione non valida";   
    }
    return 0;
